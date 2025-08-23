5415f1f425d0bf5f916f6e6da3f006425ae1f0cd
Pratham Goyal <pr648260@gmail.com>
2025-08-23 11:52:59 +0530

    done

--- ass/q1.cpp
+++ ass/q1.cpp
@@ -1,5 +1,33 @@
-#include<iostream>>
+#include <iostream>
 using namespace std;
-int main () {
-    cout<<"nb";
+int binarysearch(int arr[], int n, int k, int low, int high)
+{
+    int mid = low + (high - low) / 2; 
+    if (low<=high) {
+    if (arr[mid] == k)
+        return mid;
+    if (k > arr[mid])
+    {
+        low = mid + 1;
+    }
+    else if (k < arr[mid])
+    {
+        high = mid - 1;
+    }
+
+    binarysearch(arr, n, k, low, high);
+}
+}
+int main()
+{
+    // only for sorted array;
+    int n, k;
+    cin >> n;
+    int arr[n];
+    cin >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout<<binarysearch(arr, n, k, 0, n - 1);
 }
